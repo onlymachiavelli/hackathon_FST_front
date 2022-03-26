42a8e3a8b6d0ef9473cdda945d94a78fb93c4749
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-26 14:36:21 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Green: '',
+      },
+      fontFamily: {},
+      backgroundImage: {},
+    },
   },
   plugins: [],
 }
