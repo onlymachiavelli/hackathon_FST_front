cc96ddec47e6902efca1584cf1f845f0e40131ac
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-26 20:28:00 +0100

    rent block

--- src/pages/MDRents.tsx
+++ src/pages/MDRents.tsx
@@ -1,9 +1,20 @@
 import Layout from '../layout/ModeratorLayout'
-
+import RentCard from '../components/RentCard'
+import { Plus } from '../components/svg'
 const Moderator = () => {
   return (
     <Layout>
-      <h1>Rents CRUD</h1>
+      <h1 className="font-bold ">Rents CRUD</h1>
+      <br />
+      <button className="bg-green hover:bg-hoverGreen  text-white font-bold py-2 px-5 rounded flex items-center justify-center">
+        <Plus Width="17" /> &nbsp; Add Rent
+      </button>
+      <div className="grid gap-1 grid-cols-2 pt-4">
+        <RentCard description="lorem Lorem ipsum dolor sit amet. Et sequi omnis et voluptatem facere aut rerum minima et minus " />
+        <RentCard description="lorem Lorem ipsum dolor sit amet. Et sequi omnis et voluptatem facere aut rerum minima et minus " />
+        <RentCard description="lorem Lorem ipsum dolor sit amet. Et sequi omnis et voluptatem facere aut rerum minima et minus " />
+        <RentCard description="lorem Lorem ipsum dolor sit amet. Et sequi omnis et voluptatem facere aut rerum minima et minus " />
+      </div>
     </Layout>
   )
 }
