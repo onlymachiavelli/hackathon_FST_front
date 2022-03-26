862142618d5ba4277d7777e7d200f9a5aab00292
Nie-Mand <chill.out.x19@gmail.com>
2022-03-26 20:00:15 +0100

    porgress

--- src/components/right_login.tsx
+++ src/components/right_login.tsx
@@ -1,29 +1,65 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from 'react-router-dom'
+import { useUser } from '../store'
 
-export const Right = () =>{
-    return (
-        <div className="w-full  ">
-            <form >
+export const Right = () => {
+  const store = useUser()
+
+  const push = useNavigate()
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const data = {
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+    }
+
+    await store.launchLogin(data.email, data.password, () => {
+      push('/me')
+    })
+  }
+
+  return (
+    <div className="w-full  ">
+      <form method="post" onSubmit={handleSubmit}>
         <div className="mb-6">
-            <label  className="block mb-2 text-sm font-medium text-gray-900 ">Your Email</label>
-            <input type="email" id="email" className="outline-none bg-white border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="name@flowbite.com" required />
+          <label className="block mb-2 text-sm font-medium text-gray-900 ">
+            Your Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            className="outline-none bg-white border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+            placeholder="name@flowbite.com"
+            required
+          />
         </div>
         <div className="mb-6">
-            <label  className="block mb-2 text-sm font-medium text-gray-900 ">Your Password</label>
-            <input placeholder="Password" type="password" id="password" className="outline-none bg-white border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " required />
+          <label className="block mb-2 text-sm font-medium text-gray-900 ">
+            Your Password
+          </label>
+          <input
+            placeholder="Password"
+            type="password"
+            name="password"
+            className="outline-none bg-white border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  "
+            required
+          />
         </div>
         <div className="flex items-start mb-6">
-            
-            <div className="ml-3 text-sm">
-            </div>
+          <div className="ml-3 text-sm text-red-500">{store.login.error}</div>
         </div>
-        <button type="submit" className="text-white bg-green  focus:ring-4 focus:outline-none  font-medium rounded-sm text-sm w-full sm:w-auto px-5 py-2.5 text-center  " >Submit</button>
-            <br/>
-            <br/>
-            <Link to="/signup">You don't have an account ? </Link>
-        </form>
-        </div>
-    )
+        <button
+          type="submit"
+          className="text-white bg-green  focus:ring-4 focus:outline-none  font-medium rounded-sm text-sm w-full sm:w-auto px-5 py-2.5 text-center  "
+        >
+          Submit
+        </button>
+        <br />
+        <br />
+        <Link to="/signup">You don't have an account ? </Link>
+      </form>
+    </div>
+  )
 }
 
 export default Right
--- src/store/services.ts
+++ src/store/services.ts
@@ -1,6 +1,16 @@
 import axios from './axios'
 
 export const login = async (email: string, password: string) => {
-  const { data } = await axios.post('/login', { email, password })
+  const { data } = await axios.post('/auth/login', { email, password })
+  return data
+}
+
+export const register = async (data: any) => {
+  const { data: user } = await axios.post('/register', data)
+  return user
+}
+
+export const getMe = async () => {
+  const { data } = await axios.get('/users/getme')
   return data
 }
