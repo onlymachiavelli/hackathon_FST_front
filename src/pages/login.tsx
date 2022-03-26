4ff63510b317793debba39e1af7f45f2decd3142
Nie-Mand <chill.out.x19@gmail.com>
2022-03-26 16:28:56 +0100

    fixed forms

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,19 @@
-import Left from "../components/left_login"
-import Right from "../components/right_login"
-const Signin = () =>{
-    return (
-        <div className="w-full h-screen grid place-content-center grid-cols-2  ">
-            <aside className="w-full h-screen bg-white ">
-                <Left/>
-            </aside>
-            <aside className="w-full h-screen bg-gray-100 grid place-content-center">
-                <Right/>    
-            </aside>
-        </div>
-    )
+import Left from '../components/left_login'
+import Right from '../components/right_login'
+const Signin = () => {
+  return (
+    <div className="w-full h-screen grid place-content-center grid-cols-2  ">
+      <aside className="w-full h-screen bg-white ">
+        <Left />
+      </aside>
+      <div className="bg-white/50 w-full p-20">
+        <p className="font-bold  text-gray-900 text-3xl text-center ">
+          Create Account
+        </p>
+        <Right />
+      </div>
+    </div>
+  )
 }
 
 export default Signin
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,17 +1,22 @@
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 import Form from '../components/formSignUp'
-const Signup = () =>{
-    useEffect(() => {
-        document.title = "Create an account"
-    }, [])
-    return (
-        <div className="w-full h-screen bg-white p-4">
-            <p className="font-bold  text-gray-900 text-3xl text-center ">Create Account</p>
-            <div className='w-4/5 h-auto bg-white/50 m-auto'>
-                <Form/>
-            </div>
-        </div>  
-    )
+const Signup = () => {
+  useEffect(() => {
+    document.title = 'Create an account'
+  }, [])
+  return (
+    <div className="w-full h-screen bg-white p-4">
+      <div className="grid grid-cols-2">
+        <div></div>
+        <div className="bg-white/50 w-full p-20">
+          <p className="font-bold  text-gray-900 text-3xl text-center ">
+            Create Account
+          </p>
+          <Form />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Signup
