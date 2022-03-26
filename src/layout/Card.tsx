a90ae12b8f8055af873e2a4ea3741ef6e40c7555
adamlahbib <adem99.lhb@gmail.com>
2022-03-26 18:02:42 +0100

    md dashboard

--- src/layout/Card.tsx
+++ src/layout/Card.tsx
@@ -1,17 +1,15 @@
-const Card = () =>{
+const Card = ({...props}) =>{
     return (
         <article className="cursor-pointer border rounded-md p-3 bg-white flex text-gray-700 mb-2 hover:border-green-500 focus:outline-none focus:border-green-500">
         <div className="flex-1">
-        <header className="mb-1">
-          Tarun T <span>commented</span> on
-          <h1 className="inline">"RE: WPMS issue".</h1>
+        <header className="font-bold mb-1">
+          <h1 className="inline">{props.title}</h1>
         </header>
         <p className="text-gray-600">
-          Hi Mazhar, Please note this issue comes when user is not
-          closing or logout sy…
+          {props.body}
         </p>
         <footer className="text-gray-500 mt-2 text-sm">
-          Friday 22:16
+          {props.footer}
         </footer>
       </div>
     </article>
