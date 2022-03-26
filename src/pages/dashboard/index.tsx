6f61f2bb1a026e53179245c93f207a3ce36e5f44
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-26 15:56:10 +0100

    sidebar shit

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "../../components/sidebar"
+import Sidebar from "../../layout/sidebar"
 
 const Dashboard = () =>{
     return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,8 @@ module.exports = {
     extend: {
       colors: {
         green: '#38743d',
-<<<<<<< HEAD
-        grey: '',
-=======
         yellow: '#dbcd5c',
-        white: '#eeeeec'
->>>>>>> bc9c3b0467063d74f78d1daeff6a4a1a12e4c99e
+        white: '#eeeeec',
       },
       fontFamily: {
         sans: ['Work Sans', 'sans-serif'],
