3d9ae4a36b03cfa91c479527b7503e775fecdfda
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-26 16:21:46 +0100

    signup shits

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,7 +4,7 @@ const Dashboard = () =>{
     return (
         <div className="h-full w-full flex overflow-hidden antialiased text-gray-800 bg-white">
             <Sidebar/>
-            </div>
+        </div>
     )
 }
 
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,13 +1,16 @@
+import {useEffect} from 'react'
+import Form from '../components/formSignUp'
 const Signup = () =>{
+    useEffect(() => {
+        document.title = "Create an account"
+    }, [])
     return (
-        <div className="w-full h-screen">
-            <aside>
-
-            </aside>
-            <aside>
-                
-            </aside>
-        </div>
+        <div className="w-full h-screen bg-white p-4">
+            <p className="font-bold  text-gray-900 text-3xl text-center ">Create Account</p>
+            <div className='w-4/5 h-auto bg-white/50 m-auto'>
+                <Form/>
+            </div>
+        </div>  
     )
 }
 
