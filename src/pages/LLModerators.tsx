d1d5e50628e59fca81057d96384c78a8989616f8
Nie-Mand <chill.out.x19@gmail.com>
2022-03-26 17:05:49 +0100

    few changes

--- src/pages/LLModerators.tsx
+++ src/pages/LLModerators.tsx
@@ -3,7 +3,49 @@ import Layout from '../layout/LandlordLayout'
 const LandLoard = () => {
   return (
     <Layout>
-      <h1>Moderators CRUD</h1>
+      <h1 className="font-bold p-3 text-2xl">Moderators CRUD</h1>
+
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Product name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Color
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Category
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Price
+            </th>
+            <th scope="col" className="px-6 py-3"></th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr className="bg-white border-b">
+            <th
+              scope="row"
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+            >
+              Apple MacBook Pro 17"
+            </th>
+            <td className="px-6 py-4">Sliver</td>
+            <td className="px-6 py-4">Laptop</td>
+            <td className="px-6 py-4">$2999</td>
+            <td className="px-6 py-4 text-right">
+              <a
+                href="#"
+                className="font-medium text-blue-600  hover:underline"
+              >
+                Edit
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </Layout>
   )
 }
