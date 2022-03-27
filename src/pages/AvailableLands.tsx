30a473b131ebf6968b3f07a1329d0202a2066c69
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-27 01:38:47 +0100

    lands U9I

--- src/pages/AvailableLands.tsx
+++ src/pages/AvailableLands.tsx
@@ -1,5 +1,31 @@
+import LandCard from '../components/LandsCard'
+import { Money, Feed, Send } from '../components/svg'
+import TopBar from '../layout/Moderator/TopBar'
 const AvailableLands = () => {
-  return <div>AvailableLands</div>
+  return (
+    <div>
+      <TopBar />
+
+      <div className="w-full flex items-center justify-center ">
+        <div className="bg-[white] w-3/4 ">
+          <br />
+
+          <p className="text-center text-gray-900 font-bold text-2xl pt-10">
+            LANDS
+          </p>
+          <div className="grid grid-cols-2 w-full gap-2  place-items-center py-4">
+            <LandCard fullname={'Only Machiavelli'} />
+
+            <LandCard fullname={'Only Machiavelli'} />
+            <LandCard fullname={'Only Machiavelli'} />
+            <LandCard fullname={'Only Machiavelli'} />
+            <LandCard fullname={'Only Machiavelli'} />
+            <LandCard fullname={'Only Machiavelli'} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default AvailableLands
