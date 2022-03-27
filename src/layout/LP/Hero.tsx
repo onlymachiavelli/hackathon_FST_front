9850988b7a319bf04a97691ec4eb08e25281eb28
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-27 03:32:00 +0000

    card

--- src/layout/LP/Hero.tsx
+++ src/layout/LP/Hero.tsx
@@ -1,39 +1,44 @@
-const Hero = () =>{
-
-    return (
+const Hero = () => {
+  return (
     <>
-    <div className="bg-red-200 sticky top-0">
-                
-
+      <div className="bg-red-200 sticky top-0"></div>
+      <section className="bg-red-200 text-green-900 relative">
+        <div
+          id="heroimg"
+          className="min-h-screen hero-image bg-right-bottom bg-cover flex"
+        >
+          <div className="relative container mx-auto p-4 flex items-end z-10">
+            <div>
+              <div className="content float-left py-4 px-5 my-5">
+                <div className="heading mb-10 text-6xl">
+                  Meet LandHippo, The blah blah blah
                 </div>
-    <section className="bg-red-200 text-green-900 relative">
-    
-      <div id="heroimg" className="min-h-screen hero-image bg-right-bottom bg-cover flex">
-        <div className="relative container mx-auto p-4 flex items-end z-10">
-          <div>
-            <div className="content float-left py-4 px-5 my-5">
-              <div className="heading mb-10 text-6xl">Meet LandHippo, The blah blah blah</div>
-              <div className="text-2xl mb-10 leading-normal hidden sm:block">Affordable Properties From A Landlord You Trust!</div>
-            </div>
-            <div className="cta clear-left px-5">
-              <a className="
+                <div className="text-2xl mb-10 leading-normal hidden sm:block">
+                  Affordable Properties From A Landlord You Trust!
+                </div>
+              </div>
+              <div className="cta clear-left px-5">
+                <a
+                  className="
                     no-underline
                     btn btn-primary 
                     block sm:inline-block global-transition
-                    text-white" href="" target="">
-                <div className="flex justify-center items-center">
-                  <span>Join Alpha Today</span>
-                  <span className="fa fa-xs fa-arrow-right ml-4"></span>
-                </div>
-              </a>
+                    text-white"
+                  href=""
+                  target=""
+                >
+                  <div className="flex justify-center items-center">
+                    <span>Join Alpha Today</span>
+                    <span className="fa fa-xs fa-arrow-right ml-4"></span>
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
-    )
+  )
 }
-
 
 export default Hero
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import CardHome from '../components/Home/Cards'
-import { Managements } from '../components/svg'
+import { Managements, Safe, Agile } from '../components/svg'
 import TopBar from '../layout/LP/TopBar'
 import Hero from '../layout/LP/Hero'
 const Home = () => {
@@ -7,6 +7,12 @@ const Home = () => {
     <div className="w-full ">
       <TopBar />
       <Hero />
+      <br />
+      <br />
+      <p className="text-center px-10 font-bold text-green text-3xl">
+        OUR GOALS
+      </p>
+      <br />
       <div className="flex items-center justify-center">
         <div className="grid grid-cols-3 gap-5 w-3/4 ">
           <CardHome
@@ -15,18 +21,20 @@ const Home = () => {
             svg={<Managements Width="60" />}
           />
           <CardHome
-            title="Easy Managements"
+            title="Agile"
             description="Lorem ipsum dolor sit amet. Aut omnis tempore eum consequatur harum ab "
-            svg={<Managements Width="60" />}
+            svg={<Agile Width="50" />}
           />
 
           <CardHome
-            title="Easy Managements"
+            title="Safe Online Payement"
             description="Lorem ipsum dolor sit amet. Aut omnis tempore eum consequatur harum ab "
-            svg={<Managements Width="60" />}
+            svg={<Safe Width="60" />}
           />
         </div>
       </div>
+      <br />
+      <br />
     </div>
   )
 }
