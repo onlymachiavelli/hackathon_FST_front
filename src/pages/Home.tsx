f7963318c5fc26f22d3c93a8940e28f7ef4b594a
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-27 03:01:24 +0000

    UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
+import CardHome from '../components/Home/Cards'
+import TopBar from '../layout/LP/TopBar'
+import { Managements } from '../components/svg'
 const Home = () => {
   return (
-    <div>
-      
+    <div className="w-full ">
+      <TopBar />
+      <div className="grid grid-cols-3 gap-5">
+        <CardHome
+          title="Easy Managements"
+          description="Lorem ipsum dolor sit amet. Aut omnis tempore eum consequatur harum ab "
+          svg={<Managements Width="60" />}
+        />
+        <CardHome
+          title="Easy Managements"
+          description="Lorem ipsum dolor sit amet. Aut omnis tempore eum consequatur harum ab "
+          svg={<Managements Width="60" />}
+        />
+
+        <CardHome
+          title="Easy Managements"
+          description="Lorem ipsum dolor sit amet. Aut omnis tempore eum consequatur harum ab "
+          svg={<Managements Width="60" />}
+        />
+      </div>
     </div>
   )
 }
