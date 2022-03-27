1cd2241f0810e28b3e2026468beac14c2ad95449
onlymachiavelli <tmakaveli643@gmail.com>
2022-03-27 04:16:12 +0000

    home

--- src/components/Home/Cards.tsx
+++ src/components/Home/Cards.tsx
@@ -2,9 +2,9 @@ const CardHome = ({ ...props }) => {
   return (
     <div className="grid grid-cols-1 border-l-4 border-green px-5 bg-[white] py-10 shadow border rounded">
       <div className="m-auto">{props.svg}</div>
-      <p className="font-bold text-green text-center p-4">{props.title}</p>
+      <p className="font-bold text-gray-900 text-center p-4">{props.title}</p>
 
-      <p>{props.description}</p>
+      <p className="text-gray-900">{props.description}</p>
     </div>
   )
 }
